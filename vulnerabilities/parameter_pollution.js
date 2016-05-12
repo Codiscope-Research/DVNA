0d40eb79e8bc46da0dab58b6bcb39feca865abdf
John Kozyrakis <ikoz@users.noreply.github.com>
2016-05-12 11:20:43 +0100

    Update parameter_pollution.js

--- vulnerabilities/parameter_pollution.js
+++ vulnerabilities/parameter_pollution.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 app.get('/', function(req, res) {
-  var resp=eval("("+req.query.target+"[URL](javascript&#58document;alert&#40;1&#41;)");
+  var resp=eval("("+req.query.target+"</code>[URL](javascript&#58document;alert&#40;1&#41;)<code>");
   res.send('Parameter Value:</br>'+resp);
   });
 app.listen(9000);
